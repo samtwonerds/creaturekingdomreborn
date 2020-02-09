1cc6b2d8d4351eb7000ee06aa762482881be04f0
samtwonerds <sam@twonerds.co.uk>
2020-02-09 20:25:23 +0000

    Initial setup

---
+++ Assets/BaseEnemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour
+{
+    [Header("Core Stats")]
+    public string unitName;
+    public int unitLevel;
+    public int unitRank;
+
+    public int strength;
+    public int defence;
+    public int agility;
+    public int critChance;
+    public int critDamage;
+    public int evasion;
+    public int accuracy;
+
+    public float maxHP;
+    public float currentHP;
+
+    public float damage;
+    public float heal;
+
+    public enum Element { FIRE, ICE, NATURE, ELECTRIC, WATER, COMBAT, ARCANE }
+    public enum Rarity { COMMON, RARE, EPIC, LEGENDARY }
+
+    public Element element;
+    public Rarity rarity;
+
+    [Header("General")]
+    [SerializeField] GameObject ability1; // this should be changed to an ability class maybe???
+    [SerializeField] GameObject ability2; // use a list
+    [SerializeField] GameObject ability3;
+
+
+    public bool TakeDamage(int dmgAmount)
+    {
+        currentHP -= dmgAmount; // subtracts dmg from current HP.. apparently
+
+        if (currentHP <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHP += healAmount;
+        if (currentHP > maxHP)
+        {
+            currentHP = maxHP;
+        }
+    }
+}
---
+++ Assets/Legacy/BattleHud.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHud : MonoBehaviour
+{
+    public Text nameText;
+    public Text levelText;
+    public Slider hpSlider;
+
+    public void SetHud(BaseHero unit) // calling the unit script and naming it unit
+    {
+        nameText.text = unit.unitName; // calling the unitName variable from the Unit class
+        levelText.text = "Lvl " + unit.unitLevel;
+        hpSlider.maxValue = unit.maxHP;
+        hpSlider.value = unit.currentHP;
+
+    }
+
+    public void SetHP(int hp)
+    {
+        hpSlider.value = hp;
+    }
+}
